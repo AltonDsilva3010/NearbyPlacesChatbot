f26d4e4918b476c3dd5d55ef040176c45cada118
Alton <altondsilva3010@gmail.com>
2022-04-12 22:16:50 +0530

    last commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from unicodedata import category
 from twilio.twiml.messaging_response import MessagingResponse
 from flask import Flask,request,redirect
 import requests
@@ -21,7 +22,7 @@ def sms_reply():
 
     msg = resp.message()
 
-    def extract_lat_lng(address_or_postalcode):
+    def extract_lat_lng(address_or_postalcode): #getting lat long function
         endpoint = "https://api.geoapify.com/v1/geocode/search"
         params = {"text": address_or_postalcode, "apiKey": api_key}
         url_params = urlencode(params)
@@ -38,18 +39,53 @@ def sms_reply():
            lon = r.json()['features'][0]['properties']['lon']
         except:
           pass
-        str_lat = str(lat)
-        str_lon = str(lon)
-    
+        return lat,lon
 
-        return(str_lat +" "+ str_lon)
+    def extract_details(lat_lng,category,radius=3000):
     
+        if (category =="hospital"):
+            cat = "healthcare.hospital"
+        elif (category == "supermarket"):
+            cat = "commercial"
+        elif (category == "restuarant"):
+            cat = "catering"
+        else:
+            return "Invalid Input"
+    
+    
+        endpoint2 = "https://api.geoapify.com/v2/places"
+        bias_str = f"proximity:{lat_lng[1]},{lat_lng[0]}"
+    
+        params2 = {"categories": cat, "filter":f"circle:{lat_lng[1]},{lat_lng[0]},{radius}", "bias":bias_str, "limit":10, "apiKey": api_key}
+        url_params2 = urlencode(params2)
+        url2 = f"{endpoint2}?{url_params2}"
+        
+        r = requests.get(url2)
+        if r.status_code not in range(200, 299): 
+            return {}
+    
+        names_dict = {} 
+        try:
+            for i in range (0,10):
+                name = r.json()['features'][i]['properties']['name']
+                address = r.json()['features'][i]['properties']['formatted']
+                names_dict.update({name:address})
+        except:
+            pass
+    
+        return names_dict
   
     if incoming_msg[0] == '(':
-        msg.body(extract_lat_lng("Andheri, Mumbai"))
+        incoming_msg = incoming_msg[1:-1]
+        stringlist = incoming_msg.split(",",2)
+        category = stringlist[0]
+        radius = stringlist[1]
+        location = stringlist[2]     
+        lat_lng = extract_lat_lng(location)
+        msg.body(extract_details(lat_lng,category,radius))
      
     if 'service' in incoming_msg:
-        msg.body('Enter in the format "(type,location,radius)"')
+        msg.body('Enter in the format "(type,radius,location)"')
     
     if 'hello' in incoming_msg:
         msg.body("Hello, this is the NearBy Places Bot \n Type 'service' to get places ")
